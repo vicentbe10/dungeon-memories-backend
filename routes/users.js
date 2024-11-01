2d475c1ce36a6ae883d3ee2a778bf0c1d80b3815
vicentbe10 <vicentbe10@gmail.com>
2024-11-01 23:40:59 +0900

    fix: solve user creation issue typo2

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')[environment];
+const knex = require('knex')(config);
 const bcrypt = require('bcrypt');
 
 // Register a new user
