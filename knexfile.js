e80c862cf073932d0cec8d8148d139d40067e278
vicentbe10 <vicentbe10@gmail.com>
2024-11-01 20:38:40 +0900

    fix: update knex environment for prod. More changes2

--- knexfile.js
+++ knexfile.js
@@ -41,9 +41,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL,
-    }
+    connection: process.env.DATABASE_URL
   }
 
 };
