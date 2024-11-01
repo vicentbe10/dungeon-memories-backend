072bc923654a766c954ea3505921b96b5adf2de1
vicentbe10 <vicentbe10@gmail.com>
2024-11-01 20:24:40 +0900

    fix: update knex environment for prod. test hardcoding

--- knex.js
+++ knex.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const environment = process.env.ENVIRONMENT || "development";
+// const environment = process.env.ENVIRONMENT || "development";
+const environment = process.env.ENVIRONMENT || "production";
 const config = require("./knexfile")[environment];
 
 module.exports = require("knex")(config);
