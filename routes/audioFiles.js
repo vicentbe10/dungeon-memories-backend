49b7af537aa69fa83f68637799ba950a6fc15231
vicentbe10 <vicentbe10@gmail.com>
2024-11-02 00:22:59 +0900

    fix: more control logs

--- routes/audioFiles.js
+++ routes/audioFiles.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const fs = require("fs");
 
 // TO DO Middleware for authentication
 // const authenticate = require('../middleware/authenticate');
@@ -8,6 +9,17 @@ const router = express.Router();
 router.get('/download/:filename', /* authenticate, */ (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(__dirname, '..', 'recordings', filename);
+  
+  console.log('__dirname:', __dirname);
+  console.log('filePath:', filePath);
+
+  //Check if the file exists
+  fs.access(filePath, fs.constants.R_OK, (err) => {
+    if (err) {
+      console.error("File not found or inaccessible: ", err);
+      return res.status(404).send("File not found.");
+    }
+  })
 
   // Security check: Ensure the file exists and the user has permission to access it
   // Maybe even checking the database to verify ownership
@@ -15,7 +27,7 @@ router.get('/download/:filename', /* authenticate, */ (req, res) => {
   res.download(filePath, filename, (err) => {
     if (err) {
       console.error('Error downloading file:', err);
-      res.status(500).send('Error downloading file.');
+      res.status(500).send('Error downloading file: ', err.message);
     }
   });
 });
